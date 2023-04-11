955332bf8be2d488ef01086013f1a5465c54cd86
Eduvieira-1 <edu.vieira1@icloud.com>
2023-04-11 10:53:47 -0300

    Projeto finalizado

---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: {{ idade }}</p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome(CallBack)</button>
+    </div>
+</template>
+
+<script>
+import barramento from '@/barramento'
+
+
+export default {
+    props: {
+        nome: {
+            type: String,
+            default: 'Anonimo'
+            // required: true
+            // default: function(){
+            //     return Array(10).fill(0).join(',')
+            // }
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods:{
+        inverterNome(){
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            // this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        }
+    },
+    created(){
+        barramento.quandoIdadeMudar(idade => {
+            this.idade = idade
+        })
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
